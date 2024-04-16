ac0cb801fda3a515642bc94cc10deec0f62b926f
agemeda <asantigemeda@gmail.com>
2024-04-15 23:23:39 -0700

    two

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -7,7 +7,7 @@ echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
     echo
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13191 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    unzip -p "${file}" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13191/postgres -c "COPY tweets_jsonb(data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
@@ -15,7 +15,7 @@ echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
     echo
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:13192 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:13192 --inputs "${file}"
 done
 
 echo '================================================================================'
